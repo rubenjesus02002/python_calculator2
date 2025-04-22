cc33513b93e73ce4a1778a44e7fdd446153d4fed
avinashjairam <avinash.jairam@gmail.com>
2025-04-22 19:56:07 -0400

    coded and successfully tested introduction function

--- src/introduction.py
+++ src/introduction.py
@@ -5,7 +5,7 @@ def introduction():
 		to the average and prints an appropriate message' 
 	'''
 	print('This program prompts the user to enter 3 integers values.')
-	print('The program then finds the average of these 3 values')
-	print('It then compares them to the average and prints an appropriate message')
+	print('The program then finds the average of these 3 values.')
+	print('It then compares them to the average and prints an appropriate message.')
 
 
