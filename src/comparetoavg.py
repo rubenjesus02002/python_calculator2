b5d46c4ae3084bd3de7fa22c61cf16afaf044bb7
Musa Malik <musa.malik.farhan@gmail.com>
2025-08-03 23:51:57 -0400

    Add and test compareToAvg function for Part C

---
+++ src/comparetoavg.py
@@ -0,0 +1,13 @@
+# src/comparetoavg.py
+
+def compareToAvg(a,b,c,avg):
+	"""
+	Prints whether each number is below, equal to, or above the average.
+	"""
+	for val in [a, b, c, avg]:
+		if val < avg:
+			print(f'{val} is less than the average {avg}')
+		elif val > avg:
+			print (f'{val} is greater than the average {avg}')
+		else: 
+			print(f'{val} is equal to the average {avg}')
