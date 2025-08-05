03a8fcd22f52cd99c8f84168e4af5f1c69ebdf8b
Ruben Rodriguez <rubenatorsbaconator@MacBookPro.lan>
2025-08-04 22:57:29 -0400

    Updating main.py file to troubleshoot blank output error

--- src/main.py
+++ src/main.py
@@ -1,30 +1,29 @@
-#src/main.py
+# src/main.py
 from src.introduction import introduction
 from src.findaverage import findaverage
 from src.comparetoavg import compareToAvg
 
 def main():
     """
-    Repeats input loop ten times and prints avgerage and comparisions
+    Repeats input loop 10 times and prints average and comparisons.
     """
-
     introduction()
     num_sets = 0
 
     while num_sets < 10:
-        num1 = int(input('Enter the first integer: '))
-        num2 = int(input('Enter the second integer: '))
-        num3 = int(input('Enter the third integer: '))
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        num3 = int(input("Enter third number: "))
 
-        print()
-        print("the three original integers are:", num1, num2, num3)
         avg = findaverage(num1, num2, num3)
         print(f"The average is {avg:.3f}")
-
-        comparetoavg(num1, num2, num3, avg)
         print()
-        num_sets = num_sets + 1
+
+        compareToAvg(num1, num2, num3, avg)
+        print()
+        num_sets += 1
 
     print(f"Total sets processed: {num_sets}")
 
-
+if __name__ == "__main__":
+    main()
