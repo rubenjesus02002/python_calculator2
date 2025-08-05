07f845a3644e2825677066fd6581fe81b741606e
YuningZ02 <yuning.zhou@baruchmail.cuny.edu>
2025-08-04 21:59:07 -0400

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,26 @@
+def main():
+    """
+    Repeats input loop ten times and prints avgerage and comparisions
+    """
+
+    introduction()
+    num_sets = 0
+
+    while num_sets < 10:
+        num1 = int(input('Enter the first integer: '))
+        num2 = int(input('Enter the second integer: '))
+        num3 = int(input('Enter the third integer: '))
+
+        print()
+        print("the three original integers are:", num1, num2, num3)
+        avg = findaverage(num1, num2, num3)
+        print(f"The average is {avg:.3f}")
+
+        comparetoavg(num1, num2, num3, avg)
+        print()
+        num_sets = num_sets + 1
+
+    print(f"Total sets processed: {num_sets}")
+
+if __name__ == '__main__':
+    main()
